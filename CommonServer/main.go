9090a682f080f8434db424dd8abdfe4db2275674
BrightOlawale <oolawalebright@gmail.com>
2023-10-14 12:40:54 +0100

    fix: CORS

--- CommonServer/main.go
+++ CommonServer/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/CuesoftCloud/storefront/config"
 	"github.com/CuesoftCloud/storefront/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"os"
 )
 
 // main is the entry point for the server
 func main() {
 	config.LoadEnvVar()
 	server := gin.Default()
+	server.Use(cors.Default())
 	routes.SetupRoutes(server)
 	err := server.Run(os.Getenv("PORT"))
 	if err != nil {
