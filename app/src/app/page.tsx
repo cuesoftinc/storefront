b68c40f1300f1a1b3326a82ff026ba6c0cce00d9
bezbrain <belloquadriolawale@gmail.com>
2023-10-16 19:27:04 +0100

    Resolve all conflicts among files

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,29 @@
-import Homepage from "./home/page";
+import { Metadata } from "next";
+import Styles from "./home.module.css";
+import {
+  Adsection,
+  Brands,
+  Categories,
+  Hero,
+  Popular,
+  Services,
+  Summer,
+} from "@/components/homesections";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
 
 export default function Home() {
   return (
-    <main>
-      <Homepage />
+    <main className={Styles.main}>
+      <Hero />
+      <Brands />
+      <Categories />
+      <Popular />
+      <Summer />
+      <Services />
+      <Adsection />
     </main>
   );
 }
