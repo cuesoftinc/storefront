e6756ca50d1edc3016f9af4dca3624dbe35784c5
tamuno777 <davisbeulah07@gmail.com>
2023-10-17 04:32:38 -0700

    adjusment to summer container

--- app/src/components/homesections/summerCollection/summer.tsx
+++ app/src/components/homesections/summerCollection/summer.tsx
@@ -9,33 +9,38 @@ export default function Summer() {
   return (
     <section>
       <div className={styles.summercontainer}>
-        <div>
-          <div>
-        
+        {/* <div className={styles.summerheadercontainer}>
+                <h1 className={styles.summerheader}>
+                    Shop by caterories
+                </h1>
 
-                <div>
-                {/* <div className={styles.summerimgcontainer2}>
-                    <div className={styles.summerimg1}>
-                    <Image src={swim} alt="Register Image"className={styles.summerimgleft}/>
-                    <div className={styles.centered}>
-                    <div className={styles.text}> UP TO 80% OFF</div>
+                <a href="#" className={styles.link}> view all <span>+</span></a>
+            </div> */}
 
-                        <h2 className={styles.secondh2}>Children Summer Play set</h2>
-
-                    </div>
-                    </div>
-                  <Image src={shoe} alt="Register Image"className={styles.summerimgleft} />
-
-
-                </div> */}
-                
-            </div>
-            <div className={styles.toptext}> UP TO 80% OFF</div>
+        <div className={styles.summerimgcontainer}>
+          <div className={styles.summerimg1}>
+            <Image
+              src={sweat}
+              alt="Register Image"
+              className={styles.summerimg}
+            />
+             <div className={styles.toptext}> UP TO 80% OFF</div>
             <a href="#" className={styles.toprighttext}>
               <Summerarrow />
             </a>
-          </div>
-          <div className={styles.summerimgcontainer2}>
+            <div className={styles.buttomtext}>
+              <div className={styles.buttomheader}>
+                <h2>Kiddies Korean Summer Collection</h2>
+              </div>
+              <a href="#">
+                {" "}
+                Shop now <Shopnow />
+              </a>
+                </div>
+               
+                
+            </div>
+            <div className={styles.summerimgcontainer2}>
             <div className={styles.summerimg1}>
               <Image
                 src={swim}
@@ -54,7 +59,9 @@ export default function Summer() {
               className={styles.summerimgleft}
             />
           </div>
+           
         </div>
+         
       </div>
     </section>
   );
