e81d99c111746425e448a15b1c7a7c5b40adba6b
bezbrain <belloquadriolawale@gmail.com>
2023-10-20 15:32:26 +0100

    Nav bar mobile development

---
+++ app/src/assets/icons/navBarIcons.tsx
@@ -0,0 +1,35 @@
+export const openNav = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5"
+      />
+    </svg>
+  );
+};
+
+export const closeNave = () => {
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M6 18L18 6M6 6l12 12"
+    />
+  </svg>;
+};
