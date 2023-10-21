85a536cca729cb6fb469868e9299907071235fe8
bezbrain <belloquadriolawale@gmail.com>
2023-10-21 22:32:14 +0100

    Conditionally render the side bar

--- App/src/components/sideBar/sideBar.tsx
+++ App/src/components/sideBar/sideBar.tsx
@@ -5,15 +5,18 @@ import styles from "./sideBar.module.css";
 import InputBox from "../general/input/input";
 import { searchIcon } from "@/assets/icons";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const SideBar = () => {
+  const pathname = usePathname();
+
   const handleElectronicsSearch = () => {
     //
   };
 
   return (
     <section className={styles.side__bar}>
-      <h3>Electronics</h3>
+      <h3>{`${pathname === "/electronics" ? "Electronics" : "Fashion"}`}</h3>
       <InputBox
         type="text"
         name="search"
@@ -26,16 +29,24 @@ const SideBar = () => {
       <nav className={styles.side__bar__nav__items}>
         <ul>
           <li>
-            <Link href="">Laptops</Link>
+            <Link href="">{`${
+              pathname === "/electronics" ? "Laptops" : "Shoes"
+            }`}</Link>
           </li>
           <li>
-            <Link href="">Phones</Link>
+            <Link href="">{`${
+              pathname === "/electronics" ? "Phones" : "Men"
+            }`}</Link>
           </li>
           <li>
-            <Link href="">Accessories</Link>
+            <Link href="">{`${
+              pathname === "/electronics" ? "Accessories" : "Women"
+            }`}</Link>
           </li>
           <li>
-            <Link href="">Smart</Link>
+            <Link href="">{`${
+              pathname === "/electronics" ? "Smart TV" : "Accessories"
+            }`}</Link>
           </li>
         </ul>
       </nav>
