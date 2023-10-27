4f3927b8a0bf42d3ea2048b79899df28fff3e2cf
bezbrain <belloquadriolawale@gmail.com>
2023-10-27 20:47:31 +0100

    add all side bar nav items

---
+++ app/src/app/admin/add-items/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./add-item.module.css";
+
+const NewItems = () => {
+  return (
+    <section className={styles.add__new__items}>
+      <h2>Add New Items</h2>
+    </section>
+  );
+};
+
+export default NewItems;
