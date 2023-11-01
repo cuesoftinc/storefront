7674ac857e40c86f4066aee99e5f7be434b74375
bezbrain <belloquadriolawale@gmail.com>
2023-11-01 15:10:30 +0100

    Add the 'Add new item' buttons

--- Admin/src/components/addNewItems/addItems.tsx
+++ Admin/src/components/addNewItems/addItems.tsx
@@ -6,6 +6,7 @@ import { itemsData } from "@/data/datarafce";
 import InputBox from "../general/input/input";
 import { dropIcon } from "@/assets/iconsrafce";
 import Image from "next/image";
+import Button from "../general/button/button";
 
 const AddItems = () => {
   const handleChange = () => {
@@ -14,8 +15,16 @@ const AddItems = () => {
 
   return (
     <section className={styles.add__new__items}>
-      <h2>Add new items</h2>
-      <p>Add new available items to your shop.</p>
+      <header className={styles.add__items__header}>
+        <div>
+          <h2>Add new items</h2>
+          <p>Add new available items to your shop.</p>
+        </div>
+        <div>
+          <p>CALENDAR</p>
+        </div>
+      </header>
+
       <div className={styles.inputs__con}>
         {itemsData.map((each) => {
           return (
@@ -37,6 +46,8 @@ const AddItems = () => {
           <Image src={dropIcon} alt="Drop" width={20} height={20} />
           <p>Add Image</p>
         </div>
+        <Button btnContent="Cancel" />
+        <Button btnStyles={styles.confirm__btn} btnContent="Confirm" />
       </div>
     </section>
   );
