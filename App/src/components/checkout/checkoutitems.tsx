5084b7c167b62a4b6825cd76d51eaefd12c21b74
tamuno777 <davisbeulah07@gmail.com>
2023-11-04 08:37:30 -0700

    check1

---
+++ App/src/components/checkout/checkoutitems.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from './favourit.module.css'
+import Image from 'next/image'
+import { Tv } from '@/assets/images'
+import { Cartsvg } from '@/assets/icons'
+
+function CheckoutItems() {
+  return (
+    <div>
+        <div className={styles.favourite__card}>
+            <div className={styles.favourite__img__container}>
+                <Image src={Tv} alt='tv' className={styles.favourite__img} />
+
+            </div>
+            <div className={styles.favourite__description}>
+                <div className={styles.favourite__details}>
+                    <p>Smart Sense Pro 4k Ultra HD smart TV</p>
+                    <span> $190 </span>
+                </div>
+                <div className={styles.favourite__size__color}>
+                    <p>Color : white</p>
+                    <p>size: 41 inches</p>
+                </div>
+                <div className={styles.favourite__details}>
+                    <button className={styles.favourite__add__button} ><Cartsvg/> Add to Cart </button>
+                    <button className={styles.favourite__del__button}  >del</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Favouritecard
