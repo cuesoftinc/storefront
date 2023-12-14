72f40fe81c29e9a78e648215dd56cd8551fb71c7
bezbrain <belloquadriolawale@gmail.com>
2023-12-14 11:14:35 +0100

    Build the developer section of the home page

--- Home/src/assets/images/index.ts
+++ Home/src/assets/images/index.ts
@@ -1,3 +1,6 @@
 import storeHome from "./Home-Page.png";
+import gitMerge from "./git-merge.png";
+import codeEditior from "./code-editor.png";
+import fileJs from "./filejs.png";
 
-export { storeHome };
+export { storeHome, gitMerge, codeEditior, fileJs };
