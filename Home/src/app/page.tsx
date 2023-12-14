9445202b633ae4f7b6962f114c292fe575e19c4e
bezbrain <belloquadriolawale@gmail.com>
2023-12-14 14:38:37 +0100

    Build thee offering section closest to the footer

--- Home/src/app/page.tsx
+++ Home/src/app/page.tsx
@@ -14,11 +14,58 @@ export default function Home() {
     <main className={styles.home__page}>
       <Hero />
       <StoreHome />
-      <OurOffering />
+      <OurOffering
+        mainHeading={"Our Offerings"}
+        subHeading={"Key Features"}
+        headOne={"Discover Unique Products"}
+        headTwo={"Seamless Buying Experience"}
+        headThree={"Empower Sellers"}
+        headFour={"Customizable Storefronts"}
+        headFive={"Efficient Order Management"}
+        contentOne={
+          "Explore a curated selection of hand- picked items, ranging from hand- made crafts to cutting edge technology"
+        }
+        contentTwo={
+          "Enjoy a smooth and secure shopping journey with very easy navigation, secure transactions and personalizes recommendations"
+        }
+        contentThree={
+          "For Sellers: Showcase your items to a global audience, mange inventory effortlessly, and build a loyal customer base"
+        }
+        contentFour={
+          "Sellers can create unique stores that reflects their brand identity, making it easy for customers to connect with their products"
+        }
+        contentFive={
+          "Streamline your business operations with integrated order management, making it easy to track, process, and fulfill orders"
+        }
+      />
       <OurService />
       <ForDev />
       <OpenSource />
       <JoinUs />
+      <OurOffering
+        mainHeading={"Our Offerings"}
+        subHeading={"Storefront for Your Business"}
+        headOne={"Powerful E-commerce Solutions"}
+        headTwo={"Extendable Features"}
+        headThree={"Scalable Capabilities"}
+        headFour={"Secure User Access"}
+        headFive={"Total Control and Security"}
+        contentOne={
+          "Tailor Storefront to meet your specific business requirements. Easily adapt and scale the platform based on the unique needs of your online store"
+        }
+        contentTwo={
+          "Integrate seamlessly with your current apps, whether it’s Salesforce or React. Enhance functionalities with a smooth integration experience using Storefront"
+        }
+        contentThree={
+          "Leverage Role-Based Access Control (RBAC) with customizable rules. Benefit from audit logging, single sign-on, back up, authentication integration, etc"
+        }
+        contentFour={
+          "Ensure user authentication with secure sign-ups and logins. Utilize Google and GitHub Auth, SAML, or OIDC from popular providers for more security"
+        }
+        contentFive={
+          "Host Storefront securely on your terms for total control, privacy, and data  security. Enjoy full access to internal data sources and APIs"
+        }
+      />
     </main>
   );
 }
--- Home/src/components/home/ourOfferings/ourOffering.tsx
+++ Home/src/components/home/ourOfferings/ourOffering.tsx
@@ -1,47 +1,60 @@
 import React from "react";
 import styles from "./offerings.module.css";
 
-const OurOffering = () => {
+interface OfferingProps {
+  mainHeading: string;
+  subHeading: string;
+  headOne: string;
+  headTwo: string;
+  headThree: string;
+  headFour: string;
+  headFive: string;
+  contentOne: string;
+  contentTwo: string;
+  contentThree: string;
+  contentFour: string;
+  contentFive: string;
+}
+
+const OurOffering = ({
+  mainHeading,
+  subHeading,
+  headOne,
+  headTwo,
+  headThree,
+  headFour,
+  headFive,
+  contentOne,
+  contentTwo,
+  contentThree,
+  contentFour,
+  contentFive,
+}: OfferingProps) => {
   return (
     <section className={styles.offering__section}>
-      <h3>Our Offerings</h3>
-      <h2>Key Features</h2>
+      <h3>{mainHeading}</h3>
+      <h2>{subHeading}</h2>
 
       <div className={styles.all__features}>
         <div className={styles.feature__con}>
-          <h4>Discover Unique Products</h4>
-          <p>
-            Explore a curated selection of hand- picked items, ranging from
-            hand- made crafts to cutting edge technology
-          </p>
+          <h4>{headOne}</h4>
+          <p>{contentOne}</p>
         </div>
         <div className={styles.feature__con}>
-          <h4>Seamless Buying Experience</h4>
-          <p>
-            Enjoy a smooth and secure shopping journey with very easy
-            navigation, secure transactions and personalizes recommendations
-          </p>
+          <h4>{headTwo}</h4>
+          <p>{contentTwo}</p>
         </div>
         <div className={styles.feature__con}>
-          <h4>Empower Sellers</h4>
-          <p>
-            For Sellers: Showcase your items to a global audience, mange
-            inventory effortlessly, and build a loyal customer base
-          </p>
+          <h4>{headThree}</h4>
+          <p>{contentThree}</p>
         </div>
         <div className={styles.feature__con}>
-          <h4>Customizable Storefronts</h4>
-          <p>
-            Sellers can create unique stores that reflects their brand identity,
-            making it easy for customers to connect with their products
-          </p>
+          <h4>{headFour}</h4>
+          <p>{contentFour}</p>
         </div>
         <div className={styles.feature__con}>
-          <h4>Efficient Order Management</h4>
-          <p>
-            Streamline your business operations with integrated order
-            management, making it easy to track, process, and fulfill orders
-          </p>
+          <h4>{headFive}</h4>
+          <p>{contentFive}</p>
         </div>
       </div>
     </section>
