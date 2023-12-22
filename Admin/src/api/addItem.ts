7b28cef1d3bd84b34d2a5e0df02fef16d444745a
tamuno777 <davisbeulah07@gmail.com>
2023-12-22 03:51:36 -0800

    admin

--- Admin/src/api/addItem.ts
+++ Admin/src/api/addItem.ts
@@ -26,77 +26,77 @@
 //     // throw error.response?.data || error.message || 'An error occurred';
 //   }
 // };
-// import axios, { AxiosError } from 'axios';
+import axios, { AxiosError } from 'axios';
 
-// const BASE_URL = 'http://localhost:5005/';
-// const token = localStorage.getItem('authToken');
-
-// console.log(token);
-
-// const api = axios.create({
-//   baseURL: BASE_URL,
-//   headers: {
-//     Authorization: `Bearer ${token}`,
-//   },
-//   withCredentials: true,
-// });
-
-
-// export const addItem = async (addProduct: any) => {
-//   try {
-//     // Optional additional checks or validations
-
-//     const response = await api.post('/api/admin/create', addProduct);
-
-//     // Process successful response
-
-//     return response.data;
-//   } catch (error:any) {
-//     console.error(error); // Log detailed error info for debugging
-
-//     if (error.response && error.response.status === 403) {
-//       // Handle CORS error specifically
-//       alert('Unauthorized access. Please check your credentials.');
-//     } else {
-//       // Generic error handling
-//       alert('An error occurred. Please try again later.');
-//     }
-//   }
-// };
-const BASE_URL = 'http://localhost:5005';
+const BASE_URL = 'http://localhost:5005/';
 const token = localStorage.getItem('authToken');
 
 console.log(token);
 
-export const addItem = async (addProduct: any): Promise<any> => {
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+  withCredentials: true,
+});
+
+
+export const addItem = async (addProduct: any) => {
   try {
     // Optional additional checks or validations
-    console.log(JSON.stringify(addProduct));
-    const response = await fetch(`${BASE_URL}/api/admin/create`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(addProduct),
-    });
-console.log(response)
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+
+    const response = await api.post('/api/admin/create', addProduct);
 
     // Process successful response
-    const responseData = await response.json();
-    return responseData;
-  } catch (error) {
+
+    return response.data;
+  } catch (error:any) {
     console.error(error); // Log detailed error info for debugging
 
-    if (error instanceof TypeError && error.message.includes('NetworkError')) {
-      // Handle network error
-      alert('Network error. Please check your internet connection.');
+    if (error.response && error.response.status === 403) {
+      // Handle CORS error specifically
+      alert('Unauthorized access. Please check your credentials.');
     } else {
       // Generic error handling
       alert('An error occurred. Please try again later.');
     }
   }
 };
+// const BASE_URL = 'http://localhost:5005';
+// const token = localStorage.getItem('authToken');
+
+// console.log(token);
+
+// export const addItem = async (addProduct: any): Promise<any> => {
+//   try {
+//     // Optional additional checks or validations
+//     console.log(JSON.stringify(addProduct));
+//     const response = await fetch(`${BASE_URL}/api/admin/create`, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: `Bearer ${token}`,
+//       },
+//       body: JSON.stringify(addProduct),
+//     });
+// console.log(response)
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+
+//     // Process successful response
+//     const responseData = await response.json();
+//     return responseData;
+//   } catch (error) {
+//     console.error(error); // Log detailed error info for debugging
+
+//     if (error instanceof TypeError && error.message.includes('NetworkError')) {
+//       // Handle network error
+//       alert('Network error. Please check your internet connection.');
+//     } else {
+//       // Generic error handling
+//       alert('An error occurred. Please try again later.');
+//     }
+//   }
+// };
